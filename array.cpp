90fc2ed9e0bc124f1c5e12870c706b6372257973
purushotam12345 <purushotampatel96@gmail.com>
2024-01-26 12:14:47 +0530

    check_array

---
+++ array.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int marks[4] = {34,56,78,90};
+ cout<<"math"<<marks[0]<<endl;
+ cout<<"science"<<marks[1]<<endl;
+ cout<<"scial"<<marks[2]<<endl;
+ cout<<"os"<<marks[3]<<endl;
+ return 0;   
+}
